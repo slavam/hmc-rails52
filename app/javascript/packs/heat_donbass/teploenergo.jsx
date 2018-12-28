491827ef1fcc80593cefb3ab22039a6543cf7630
slavam1 <mwm1955@gmail.com>
2018-12-28 08:13:53 +0000

    Добовил график в теплоэнерго, сделал контроль гидро телеграмм

--- app/javascript/packs/heat_donbass/teploenergo.jsx
+++ app/javascript/packs/heat_donbass/teploenergo.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TeploenergoForm from './teploenergo_form';
+import {Line} from 'react-chartjs-2';
 
 const AvgTemperatures = ({temperatures, maxDay}) => {
-  let row = [<tr><td>Число</td><td>Донецк</td><td>Дебальцево</td><td>Амвросиевка</td><td>Волноваха</td><td>Мариуполь</td></tr>];
+  let row = [<tr key="0"><td>Число</td><td>Донецк</td><td>Дебальцево</td><td>Амвросиевка</td><td>Волноваха</td><td>Мариуполь</td></tr>];
   let values = [];
   for(var i=1; i<=maxDay; ++i){
     values = [];
     [1,3,2,4,5].forEach((j) => { // коды станций 1-5
       let key = ('0'+i).slice(-2)+'-'+j;
       let val = temperatures[key] == null ? '': temperatures[key];
-      values.push(<td>{val}</td>);
+      values.push(<td key={j}>{val}</td>);
     });
-    row.push(<tr key={i}><td>{i}</td>{values}</tr>);
+    row.push(<tr key={i}><td key={i}>{i}</td>{values}</tr>);
   }
   return <table className="table table-hover"><tbody>{row}</tbody></table>;
 };
@@ -46,10 +47,74 @@ export default class Teploenergo extends React.Component{
     const MONTHS = ['', 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
     let endDate = this.state.daysInMonth+' '+MONTHS[+this.state.month]+' '+this.state.year;
     let desiredLink = "/synoptic_observations/teploenergo.pdf?year="+this.state.year+"&month="+this.state.month;
+    let dataDonetsk = [];
+    Object.keys(this.state.temperatures).forEach((k) => {if(k[3]=='1') dataDonetsk[+k.substr(0,2)-1]=this.state.temperatures[k]});
+    let dataDebaltsevo = [];
+    Object.keys(this.state.temperatures).forEach((k) => {if(k[3]=='3') dataDebaltsevo[+k.substr(0,2)-1]=this.state.temperatures[k]});
+    let dataAmvrosievka = [];
+    Object.keys(this.state.temperatures).forEach((k) => {if(k[3]=='2') dataAmvrosievka[+k.substr(0,2)-1]=this.state.temperatures[k]});
+    let dataVolnovaha = [];
+    Object.keys(this.state.temperatures).forEach((k) => {if(k[3]=='4') dataVolnovaha[+k.substr(0,2)-1]=this.state.temperatures[k]});
+    let dataMariupol = [];
+    Object.keys(this.state.temperatures).forEach((k) => {if(k[3]=='5') dataMariupol[+k.substr(0,2)-1]=this.state.temperatures[k]});
+    const lineChartData = {
+      labels: Array.from({length: this.state.daysInMonth}, (v, k) => k+1),
+      datasets: [ 
+        {
+          backgroundColor: 'rgba(255,99,32,0.2)',
+          borderColor: 'rgba(255,99,32,1)',
+      	  label: "Донецк",
+      	  data: dataDonetsk,
+        	fill: false,
+        }, 
+        {
+          label: "Дебальцево",
+          backgroundColor: 'rgba(55,199,32,0.2)',
+          borderColor: 'rgba(55,199,32,1)',
+          fill: false,
+          data: dataDebaltsevo
+        },
+        {
+          label: "Амвросиевка",
+          backgroundColor: 'rgba(55,99,232,0.2)',
+          borderColor: 'rgba(55,99,232,1)',
+          fill: false,
+          data: dataAmvrosievka
+        },
+        {
+          label: "Волноваха",
+          backgroundColor: 'rgba(155,99,132,0.2)',
+          borderColor: 'rgba(155,99,132,1)',
+          fill: false,
+          data: dataVolnovaha
+        },
+        {
+          label: "Мариуполь",
+          backgroundColor: 'rgba(155,199,32,0.2)',
+          borderColor: 'rgba(155,199,232,1)',
+          fill: false,
+          data: dataMariupol
+        }
+      ]
+    };
+    let options = {
+    	responsive: true,
+	    title: {		
+	      display: false,
+		    text: 'Chart.js Line Chart'
+      },
+  		tooltips: {
+        mode: 'label'
+      },
+  		hover: {
+        mode: 'dataset'
+      },
+    };
     return(
       <div>
         <TeploenergoForm year={this.state.year} month={this.state.month} onFormSubmit={this.handleFormSubmit} />
         <h4>Средняя за сутки температура воздуха (°С) с 1 по {endDate} года по данным метеорологических станций</h4>
+        <Line data={lineChartData} height={100} options={options}/>
         <AvgTemperatures temperatures={this.state.temperatures} maxDay={this.state.daysInMonth}/>
         <a href={desiredLink}>Распечатать</a>
       </div>
@@ -63,7 +128,6 @@ $(() => {
     const temperatures = JSON.parse(node.getAttribute('temperatures'));
     const year = JSON.parse(node.getAttribute('year'));
     const month = JSON.parse(node.getAttribute('month'));
-    
     
     ReactDOM.render(
       <Teploenergo temperatures={temperatures} year={year} month={month} />,
--- app/javascript/packs/input_hydro_telegrams/check_hydro_telegram.js
+++ app/javascript/packs/input_hydro_telegrams/check_hydro_telegram.js
@@ -1,7 +1,7 @@
 export function checkHydroTelegram(tlgText, hydroPosts, errors, observation, dateObservation){
   let tlg = tlgText;
   let type = tlg.substr(0,6);
-  if((type == "ЩЭРЕХ ") || (type == 'ЩЭГОЛ ')){
+  if((type == "ЩЭРЕХ ") || (type == "ЩЭРЕА ") || (type == 'ЩЭГОЛ ')){
     observation.hydro_type = type.trim();
   } else {
     errors.push("Ошибка в различительной группе");
@@ -32,37 +32,250 @@ export function checkHydroTelegram(tlgText, hydroPosts, errors, observation, dat
     return false;
   }
   let currPos = 18;
-  if(/^1\d{4}$/.test(tlg.substr(currPos,5))){
-    currPos += 6;
-  } else {
-    errors.push("Ошибка в группе 1 раздела 1");
-    return false;
+  if (tlg[currPos] != '9')
+    if(checkSection1('1')){
+      // console.log('>>>>>>>>>>> currPos=>'+currPos);
+    } else {
+      return false;
+    }
+  let groupNum = 0;
+  if(tlg.substr(currPos-1,4) == ' 922'){
+    if(tlg[16] != '2'){
+      errors.push("Ошибка в признаке присутствия разделов =>"+tlg.substr(currPos));
+      return false;
+    }
+    groupNum = 0;
+    while (tlg.substr(currPos-1,4) == ' 922'){
+      if(+tlg.substr(currPos+3,2)>0 && +tlg.substr(currPos+3,2)<32){
+        currPos += 6;
+        groupNum += 1;
+      } else {
+        errors.push("Ошибка в номере дня в разделе 922["+groupNum+"]");
+        return false;
+      }
+      if(groupNum > 5){
+        errors.push("Раздел 2 повторяется более 5 раз");
+        return false;
+      }
+      if (checkSection1("922["+groupNum+"]")){
+        // console.log('>>>>>>>>>>> currPos=>'+currPos+'; >'+tlg.substr(currPos));
+      } else {
+        errors.push(errors[0]+" =>"+tlg.substr(currPos));
+        return false;
+      }
+    }
   }
-  if(/^2\d{3}[012]$/.test(tlg.substr(currPos,5))){
-    currPos += 6;
-  } else {
-    errors.push("Ошибка в группе 2 раздела 1");
-    return false;
-  }
-  if(tlg[currPos] == '3')
+  if(tlg.substr(currPos-1,4) == ' 966'){
+    if(tlg[16] != '2'){
+      errors.push("Ошибка в признаке присутствия разделов =>"+tlg.substr(currPos));
+      return false;
+    }
+    if(+tlg.substr(currPos+3,2)>0 && +tlg.substr(currPos+3,2)<13){
+      currPos+=6;
+    } else {
+      errors.push("Ошибка в номере месяца в разделе 966 =>"+tlg.substr(currPos));
+      return false;
+    }
+    if(/^1\d{4}$/.test(tlg.substr(currPos,5))){
+      currPos += 6;
+    } else {
+      errors.push("Ошибка в группе 1 раздела 966 =>"+tlg.substr(currPos));
+      return false;
+    }
+    if(/^2\d{4}$/.test(tlg.substr(currPos,5))){
+      currPos += 6;
+    } else {
+      errors.push("Ошибка в группе 2 раздела 966 =>"+tlg.substr(currPos));
+      return false;
+    }
     if(/^3\d{4}$/.test(tlg.substr(currPos,5))){
       currPos += 6;
     } else {
-      errors.push("Ошибка в группе 3 раздела 1");
+      errors.push("Ошибка в группе 3 раздела 966 =>"+tlg.substr(currPos));
       return false;
     }
-  if(tlg[currPos] == '4')
     if(/^4\d{4}$/.test(tlg.substr(currPos,5))){
       currPos += 6;
     } else {
-      errors.push("Ошибка в группе 4 раздела 1");
+      errors.push("Ошибка в группе 4 раздела 966 =>"+tlg.substr(currPos));
       return false;
     }
-  if(tlg[currPos] == '5'){
-    
+    if(/^5[0-3]\d[012]\d$/.test(tlg.substr(currPos,5))){
+      currPos+=6;
+    } else{
+      errors.push("Ошибка в группе 5 раздела 966 =>"+tlg.substr(currPos));
+      return false;
+    }
+    if(tlg[currPos] == '6')
+      if(/^60\d{3}$/.test(tlg.substr(currPos,5))){
+        currPos+=6;
+      }else{
+        errors.push("Ошибка в группе 6 раздела 966 =>"+tlg.substr(currPos));
+        return false;
+      }
+    if(tlg[currPos] == '7')
+      if(/^7\d{4}$/.test(tlg.substr(currPos,5))){
+        currPos+=6;
+      }else{
+        errors.push("Ошибка в группе 7 раздела 966 =>"+tlg.substr(currPos));
+        return false;
+      }
+    if(tlg[currPos] == '8')
+      if(/^8[0-3]\d[012]\d$/.test(tlg.substr(currPos,5))){
+        currPos+=6;
+      } else{
+        errors.push("Ошибка в группе 8 раздела 966 =>"+tlg.substr(currPos));
+        return false;
+      }
+    if(/^9\d{4}$/.test(tlg.substr(currPos,5))){
+      currPos+=6;
+    }else{
+      errors.push("Ошибка в группе 9 раздела 966 =>"+tlg.substr(currPos));
+      return false;
+    }
   }
-  if(tlg[currPos-1] == '=')
-    return true;
-  errors.push("Debugging");
+  if(tlg.substr(currPos-1,5) == ' 9770'){
+    if(tlg.substr(0,5) != 'ЩЭГОЛ'){
+      errors.push("Ошибка в различительной группе =>"+tlg.substr(currPos));
+      return false;
+    }
+    if(tlg[16] != '7'){
+      errors.push("Ошибка в признаке присутствия разделов =>"+tlg.substr(currPos));
+      return false;
+    }
+    if(tlg[currPos+4] == '1'){ // согласовано с АОА 20181225
+      currPos+=6;
+      if(/^1\d{4}$/.test(tlg.substr(currPos,5)))
+        currPos+=6;
+      else {
+        errors.push("Ошибка в группе 1 раздела 97701 =>"+tlg.substr(currPos));
+        return false;
+      }
+      if(/^2\d{3}[012]$/.test(tlg.substr(currPos,5))){
+        currPos += 6;
+      } else {
+        errors.push("Ошибка в группе 2 раздела 97701 =>"+tlg.substr(currPos));
+        return false;
+      }  
+      if(tlg[currPos] == '5')
+        if(/^5\d{4}$/.test(tlg.substr(currPos,5))){
+          currPos += 6;
+        } else {
+          errors.push("Ошибка в группе 5 раздела 97701");
+          return false;
+        }
+      if(tlg[currPos] == '6')
+        if(/^6\d{4}$/.test(tlg.substr(currPos,5))){
+          currPos += 6;
+        } else {
+          errors.push("Ошибка в группе 6 раздела 97701");
+          return false;
+        }
+    }
+    if(tlg[currPos+4] == '5'){
+      currPos+=6;
+      if(/^0\d{3}[0-4]$/.test(tlg.substr(currPos,5))){
+        currPos += 6;
+      } else {
+        errors.push("Ошибка в группе 0 раздела 97705");
+        return false;
+      }
+    }
+    currPos = tlg.length;
+  }
+  // if(tlg[currPos-1] == '=')
+    // return true;
+  errors.push("Debugging =>"+tlg.substr(currPos));
   return false;
+  
+  function checkSection1(sectionId){
+    if(/^1\d{4}$/.test(tlg.substr(currPos,5))){
+      currPos += 6;
+    } else {
+      errors.push("Ошибка в группе 1 раздела "+sectionId);
+      return false;
+    }
+    if(/^2\d{3}[012]$/.test(tlg.substr(currPos,5))){
+      currPos += 6;
+    } else {
+      errors.push("Ошибка в группе 2 раздела "+sectionId+" =>"+tlg.substr(currPos));
+      return false;
+    }
+    if(tlg[currPos] == '3')
+      if(/^3\d{4}$/.test(tlg.substr(currPos,5))){
+        currPos += 6;
+      } else {
+        errors.push("Ошибка в группе 3 раздела "+sectionId);
+        return false;
+      }
+    if(tlg[currPos] == '4')
+      if(/^4\d{4}$/.test(tlg.substr(currPos,5))){
+        currPos += 6;
+      } else {
+        errors.push("Ошибка в группе 4 раздела "+sectionId);
+        return false;
+      }
+    if(tlg[currPos] == '5'){
+      while (tlg[currPos] == '5'){
+        groupNum += 1;
+        if(groupNum > 5){
+          errors.push("Группа 5 раздела "+sectionId+" повторяется более 5 раз");
+          return false;
+        }
+        if(/^5\d{4}$/.test(tlg.substr(currPos,5))){
+          currPos += 6;
+        } else {
+          errors.push("Ошибка в группе 5["+groupNum+"] раздела "+sectionId);
+          return false;
+        }
+      }
+    }
+    if(tlg[currPos] == '6'){
+      groupNum = 0;
+      while (tlg[currPos] == '6'){
+        groupNum += 1;
+        if(groupNum > 5){
+          errors.push("Группа 6 раздела "+sectionId+" повторяется более 5 раз");
+          return false;
+        }
+        if(/^6\d{4}$/.test(tlg.substr(currPos,5))){
+          currPos += 6;
+        } else {
+          errors.push("Ошибка в группе 6["+groupNum+"] раздела "+sectionId);
+          return false;
+        }
+      }
+    }
+    if(tlg[currPos] == '7')
+      if(/^7\d{4}$/.test(tlg.substr(currPos,5))){
+        currPos += 6;
+      } else {
+        errors.push("Ошибка в группе 7 раздела "+sectionId);
+        return false;
+      }
+    if(tlg[currPos] == '8')
+      if(/^8\d{4}$/.test(tlg.substr(currPos,5))){
+        currPos += 6;
+      } else {
+        errors.push("Ошибка в группе 8 раздела "+sectionId);
+        return false;
+      }
+    if(tlg[currPos] == '0'){
+      if(/^0\d{3}[0-4]$/.test(tlg.substr(currPos,5))){
+        currPos += 6;
+      } else {
+        errors.push("Ошибка в группе 0 раздела "+sectionId);
+        return false;
+      }
+      if(tlg.substr(currPos,3) == '988')
+        if(/^988\d\d 0\d{3}[0-4]$/.test(tlg.substr(currPos,11))){
+          currPos += 12;
+        } else {
+          errors.push("Ошибка в группе 988 раздела "+sectionId);
+          return false;
+        }
+    }
+    return true;
+  }
+
 }
