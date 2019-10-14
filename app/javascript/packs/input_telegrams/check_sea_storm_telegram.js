9592db0be7e668c2dc1e944d2f36341cf1288194
slavam <mwm1955@gmail.com>
2019-10-14 16:28:37 +0300

    Работа над штормами на море

---
+++ app/javascript/packs/input_telegrams/check_sea_storm_telegram.js
@@ -0,0 +1,95 @@
+export function checkSeaStormTelegram(tlg, stations, errors, observation){
+  if(~tlg.indexOf("ЩЭОЯА ") || ~tlg.indexOf("ЩЭОЯУ ") ){
+    observation.telegram_type = tlg.substr(0, 5);
+  } else {
+    errors.push("Ошибка в различительной группе");
+    return false;
+  }
+  if(~tlg.indexOf(" WAREP ")){} else {
+    errors.push("Отсутствует идентификатор WAREP");
+    return false;
+  }
+  // var codeStation = tlg.substr(12,5);
+  // var isStation = false; 
+  // var idStation = -1;
+  // isStation = stations.some(function(s){
+  //   idStation = s.id;
+  //   return +codeStation == s.code;
+  // });
+  // if (isStation && (tlg[17] == ' ')) {
+  if (tlg.substr(12,6) == '99023 '){  
+    observation.station_id = 10; // Sedovo idStation;
+  } else {
+    errors.push("Ошибка в коде метеостанции");
+    return false;
+  }
+  var value;
+  value = +tlg.substr(18,2);
+  if ((value > 0) && (value < 32)){
+    observation.day_event = value;
+  } else {
+    errors.push("Ошибка в номере дня");
+    return false;
+  }
+  value = +tlg.substr(20,2);
+  if ((value >= 0) && (value < 24)){
+    observation.hour_event = value;
+  } else {
+    errors.push("Ошибка в часе явления");
+    return false;
+  }
+  value = +tlg.substr(22,2);
+  if ((value >= 0) && (value < 60)){
+    observation.minute_event = value;
+  } else {
+    errors.push("Ошибка в минутах явления");
+    return false;
+  }
+  var codeWAREP = +tlg.substr(25,2);
+  var currentPos = 28;
+  
+  if (isCodeWAREP()) {
+    if (checkByCode())
+      return true;
+      // if (tlg[currentPos-1] == '=')
+      //   return true;
+      // else {
+      //   codeWAREP = +tlg.substr(currentPos, 2);
+      //   currentPos += 3;
+      // }
+    else{
+      return false;
+    }
+  } else {
+    errors.push("Ошибочный код WAREP");
+    return false;
+  }
+  
+  function isCodeWAREP(){ 
+    return [21, 22, 23, 24, 25, 26, 27, 28, 29, 80, 81, 82].some(s => {
+      return codeWAREP == s;
+    });
+  }
+  function checkByCode(){
+    switch (codeWAREP) {
+      case 21:
+        if(/^4\d{4}$/.test(tlg.substr(currentPos,5))){
+          if(tlg[currentPos+5]=='=')
+            return true;
+          else{
+            currentPos += 6;
+            if(/^1[0-3]\d{3}$/.test(tlg.substr(currentPos,5)))
+              if(tlg[currentPos+5]=='=')
+                return true;
+              else{
+                errors.push("Ошибка в окончании телеграммы");
+                return false;
+              }
+          }
+        }else{
+          errors.push("Ошибка в группе 4");
+          return false;
+        }
+    }
+  }
+}
