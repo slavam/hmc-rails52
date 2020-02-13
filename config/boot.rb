4650efa98323074cb64f878c8aa62605304f7349
slavam <mwm1955@gmail.com>
2020-02-13 13:36:46 +0300

    Вернул в исходное состояние файл /config/boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 # make the ExecJs use NodeJs
- ENV['EXECJS_RUNTIME'] = 'Node'
+# ENV['EXECJS_RUNTIME'] = 'Node'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
