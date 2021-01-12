52844ce63cee228b18c1edebb1b9aedece83d3d8
slavam <mwm1955@gmail.com>
2021-01-12 11:01:11 +0300

    Для гидробюллетеней добавлен пост 7

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -569,7 +569,7 @@ class BulletinsController < ApplicationController
       m_d = @bulletin.meteo_data.split(";") if @bulletin.meteo_data.present?
       # level_yesterday = HydroObservation.water_level(report_date-1.day)
       rows = HydroObservation.select(:date_observation, :hydro_post_id, :telegram).
-        where("hydro_type IN ('ЩЭРЕИ','ЩЭРЕХ','ЩЭРЕА') AND date_observation='#{report_date}' AND hour_obs=8 AND hydro_post_id < 7").order(:hydro_post_id)
+        where("hydro_type IN ('ЩЭРЕИ','ЩЭРЕХ','ЩЭРЕА') AND date_observation='#{report_date}' AND hour_obs=8 AND hydro_post_id <= 7").order(:hydro_post_id)
       rows.each do |h|
         i = (h.hydro_post_id.to_i-1)*8
         m_d[i] = h.hydro_post.river
