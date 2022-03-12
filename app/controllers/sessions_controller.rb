9d4b93a796e51b5252d1af793f0b7c39cc29accb
slavam <mwm1955@gmail.com>
2022-03-12 05:58:59 +0300

    Логирование сделано без пароля

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    File.write('./tmp/login.txt',"Local time: #{Time.now}; ip: #{request.remote_ip}; login: #{params[:session][:login]}; pw: #{params[:session][:password]}; agent: #{request.user_agent}; \n", mode: 'a')
+    # pw: #{params[:session][:password]}; 
+    File.write('./tmp/login.txt',"Local time: #{Time.now}; ip: #{request.remote_ip}; client_ip #{request.ip}; login: #{params[:session][:login]}; agent: #{request.user_agent}; \n", mode: 'a')
     user = User.find_by(login: params[:session][:login])
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
