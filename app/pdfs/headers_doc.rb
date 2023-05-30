644b447ab5dec4a0708af39a7341d12c7a09f210
slavam <mwm1955@gmail.com>
2023-05-30 14:13:28 +0300

    New logo for bulletins

--- app/pdfs/headers_doc.rb
+++ app/pdfs/headers_doc.rb
@@ -1,6 +1,6 @@
 module HeadersDoc
   def bulletin_header(y_pos)
-    image "./app/assets/images/roshydromet.png", :scale => 0.085
+    image "./app/assets/images/logo2015_2.png", :scale => 0.4
     bounding_box([85, y_pos], width: bounds.width-85) do
       text Bulletin::HEAD1, align: :center, size: 10
       text Bulletin::HEAD2, align: :center, size: 10, style: :bold
