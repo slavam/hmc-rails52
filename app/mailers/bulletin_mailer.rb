0ed06e435b356a3b216fc45a07be31d09c509e16
slavam <mwm1955@gmail.com>
2025-03-03 09:10:26 +0300

    Для тестирования автодора подставлены тестовые адреса

--- app/mailers/bulletin_mailer.rb
+++ app/mailers/bulletin_mailer.rb
@@ -6,7 +6,8 @@ class BulletinMailer < ApplicationMailer
     filename = File.join(Rails.root, folder, "bulletin_for_email.pdf")
     pdf.render_file(filename)
     attachments['bulletin.pdf'] = File.read(filename)
-    recivers = Rails.env.production? ? ["mail@add-ldnr.ru", "avtodordonbassd@mail.ru"]:["morgachev@dnr.mecom.ru"]
+    # recivers = Rails.env.production? ? ["mail@add-ldnr.ru", "avtodordonbassd@mail.ru"]:["morgachev@dnr.mecom.ru"]
+    recivers = Rails.env.production? ? ["dingener@dnr.mecom.ru", "novitsiky@yandex.ru"]:["morgachev@dnr.mecom.ru"]
     mail(to: recivers, subject: "Бюллетень  № #{bulletin.curr_number} от #{bulletin.report_date.strftime("%d.%m.%Y")}")
   end
 end
