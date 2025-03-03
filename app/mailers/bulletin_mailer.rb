1b36c8c4d6680658e8c2d97e316b5f7e759035d2
slavam <mwm1955@gmail.com>
2025-03-03 11:26:48 +0300

    asset to assets changed

--- app/mailers/bulletin_mailer.rb
+++ app/mailers/bulletin_mailer.rb
@@ -2,11 +2,12 @@ class BulletinMailer < ApplicationMailer
   def autodor_email
     bulletin = params[:bulletin]
     pdf = Avtodor.new(bulletin)
-    folder = Rails.env.production? ? "public/asset/pdf" : "app/assets/pdf_folder"
+    folder = Rails.env.production? ? "public/assets/pdf" : "app/assets/pdf_folder"
     filename = File.join(Rails.root, folder, "bulletin_for_email.pdf")
+    Rails.logger.debug("My object+++++++++++++++++: #{filename}")
     pdf.render_file(filename)
     attachments['bulletin.pdf'] = File.read(filename)
-    # recivers = Rails.env.production? ? ["mail@add-ldnr.ru", "avtodordonbassd@mail.ru"]:["morgachev@dnr.mecom.ru"]
+    # recivers = Rails.env.production? ? ["dingener@dnr.mecom.ru", "mail@add-ldnr.ru", "avtodordonbassd@mail.ru"]:["morgachev@dnr.mecom.ru"]
     recivers = Rails.env.production? ? ["dingener@dnr.mecom.ru", "novitsiky@yandex.ru"]:["morgachev@dnr.mecom.ru"]
     mail(to: recivers, subject: "Бюллетень  № #{bulletin.curr_number} от #{bulletin.report_date.strftime("%d.%m.%Y")}")
   end
